0c065c8dbb9920bb44041b5c1aeb7bf222ffa3c4
dlin21 <90690772+dlin21@users.noreply.github.com>
2022-01-27 22:17:10 -0500

    Update main.c

--- main.c
+++ main.c
@@ -10,20 +10,21 @@
 
 static void sighandler(int signo){
   if (signo == SIGINT){
-    int file = open("signals.txt", O_CREAT | O_WRONLY, O_APPEND, 0644);
+    int file = open("log.txt", O_CREAT | O_WRONLY, O_APPEND, 0644);
     if(file == -1){
-      printf("%s\n", strerror(errno));
+      printf("error: %s\n", strerror(errno));
       exit(0);
     }
 
     char path[100] = "Program exited due to SIGINT\n";
-    int a = write(file, path, sizeof(path));
-    if (a == -1){
+    int err = write(file, path, sizeof(path));
+    if (err == -1){
       printf("%s\n", strerror(errno));
       return;
     }
     exit(0);
   }
+  
   if(signo == SIGUSR1) printf("PPID: %d\n", getppid());
   
 }
